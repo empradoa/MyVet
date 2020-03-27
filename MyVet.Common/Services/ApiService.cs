fbbb1c5990ad2fad54a0e156ed25c26d4b4f69f2
Eider Prado <eiderprado@hotmail.com>
2020-03-26 19:37:46 -0500

    Show histories

--- MyVet.Common/Services/ApiService.cs
+++ MyVet.Common/Services/ApiService.cs
@@ -5,12 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using MyVet.Common.Models;
 using Newtonsoft.Json;
+using Plugin.Connectivity;
 
 namespace MyVet.Common.Services
 {
     public class ApiService : IApiService
     {
-        public async Task<Response> GetTokenAsync(
+        //revisar la conexión a internet
+        public async Task<bool> CheckConnection(string url)
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                return false;
+            }
+            return await CrossConnectivity.Current.IsRemoteReachable(url);
+        }
+        public async Task<Response<TokenResponse>> GetTokenAsync(
             string urlBase,
             string servicePrefix,
             string controller,
@@ -32,7 +42,7 @@ namespace MyVet.Common.Services
 
                 if (!response.IsSuccessStatusCode)
                 {
-                    return new Response
+                    return new Response<TokenResponse>
                     {
                         IsSuccess = false,
                         Message = result,
@@ -41,7 +51,7 @@ namespace MyVet.Common.Services
 
                 //DeserializeObject es coger un string y convertilo es clase o objeto
                 var token = JsonConvert.DeserializeObject<TokenResponse>(result);
-                return new Response
+                return new Response<TokenResponse>
                 {
                     IsSuccess = true,
                     Result = token
@@ -49,7 +59,7 @@ namespace MyVet.Common.Services
             }
             catch (Exception ex)
             {
-                return new Response
+                return new Response<TokenResponse>
                 {
                     IsSuccess = false,
                     Message = ex.Message
@@ -57,7 +67,7 @@ namespace MyVet.Common.Services
             }
         }
 
-        public async Task<Response> GetOwnerByEmailAsync(
+        public async Task<Response<OwnerResponse>> GetOwnerByEmailAsync(
             string urlBase,
             string servicePrefix,
             string controller,
@@ -82,7 +92,7 @@ namespace MyVet.Common.Services
 
                 if (!response.IsSuccessStatusCode)
                 {
-                    return new Response
+                    return new Response<OwnerResponse>
                     {
                         IsSuccess = false,
                         Message = result,
@@ -90,7 +100,7 @@ namespace MyVet.Common.Services
                 }
 
                 var owner = JsonConvert.DeserializeObject<OwnerResponse>(result);
-                return new Response
+                return new Response<OwnerResponse>
                 {
                     IsSuccess = true,
                     Result = owner
@@ -98,7 +108,7 @@ namespace MyVet.Common.Services
             }
             catch (Exception ex)
             {
-                return new Response
+                return new Response<OwnerResponse>
                 {
                     IsSuccess = false,
                     Message = ex.Message
--- MyVet.Prism/MyVet.Prism/ViewModels/LoginPageViewModel.cs
+++ MyVet.Prism/MyVet.Prism/ViewModels/LoginPageViewModel.cs
@@ -67,13 +67,24 @@ namespace MyVet.Prism.ViewModels
             IsRunning = true;
             IsEnabled = false;
 
+            //hace uso del metodo de la ApiService.cs de common para verificar la conexion a internet
+
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var connection = await _apiService.CheckConnection(url);
+            if (!connection)
+            {
+                IsEnabled = true;
+                IsRunning = false;
+                await App.Current.MainPage.DisplayAlert("Error", "Check the internet connection.", "Accept");
+                return;
+            }
+
             var request = new TokenRequest
             {
                 Password = Password,
                 Username = Email
             };
 
-            var url = App.Current.Resources["UrlAPI"].ToString();
             var response = await _apiService.GetTokenAsync(url, "Account", "/CreateToken", request);
             if (!response.IsSuccess)
             {
@@ -84,7 +95,7 @@ namespace MyVet.Prism.ViewModels
                 return;
             }
 
-            var token = (TokenResponse)response.Result;
+            var token = response.Result;
             var response2 = await _apiService.GetOwnerByEmailAsync(url, "api", "/Owners/GetOwnerByEmail", "bearer", token.Token, Email);
             if (!response2.IsSuccess)
             {
@@ -94,7 +105,7 @@ namespace MyVet.Prism.ViewModels
                 return;
             }
 
-            var owner = (OwnerResponse)response2.Result;
+            var owner = response2.Result;
             var parameters = new NavigationParameters
             {
                 { "owner", owner }
@@ -102,7 +113,7 @@ namespace MyVet.Prism.ViewModels
 
             IsRunning = false;
             IsEnabled = true;
-
+                       
             //await App.Current.MainPage.DisplayAlert("Ok", "Fuck yeahh!!!", "Accept");
             await _navigationService.NavigateAsync("PetsPage",parameters);
             Password = string.Empty;
