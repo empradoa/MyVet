7c69ddaf5c60330e152b7854cd134ea99e4e7bb3
Eider Prado <eiderprado@hotmail.com>
2020-04-08 01:43:33 -0500

    Tutorial 88 ok

---
+++ MyVet.Common/Services/IGeolocatorService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace MyVet.Common.Services
+{
+    public interface IGeolocatorService
+    {
+        //lo que nos alejamos y nos acercamos a la linea del ecuador
+        double Latitude { get; set; }
+
+        //Lo que nos movemos al oriente o al occidente del meridiano de grenwichs
+        double Longitude { get; set; }
+
+        Task GetLocationAsync();
+    }
+}
